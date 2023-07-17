4594372fb0f230fda9e33c3dd2c440be95be89e6
Yuvraj Jaiswal <quicksilver92571331@gmail.com>
2023-07-17 18:41:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const org = prompt("Enter the name of Organization");
 $('.institute').text(org);
 const pos = prompt("Enter the address");
 $('.location').text(pos);
+const time=prompt("Enter the duration");
+$('.duration').text(time);
 
 $(document).ready(function() {
     $('#imageUpload1').change(function(e) {
@@ -31,4 +33,39 @@ $(document).ready(function() {
       reader.readAsDataURL(file);
     }
   });
+  
+  $(function() {
+    // Initialize the Datepicker widgets
+    $("#startDatePicker").datepicker();
+    $("#endDatePicker").datepicker();
+  
+    // Handle the change event for the start date
+    $("#startDatePicker").on("change", function() {
+      updateStartDate($(this).datepicker("getDate"));
+    });
+  
+    // Handle the change event for the end date
+    $("#endDatePicker").on("change", function() {
+      updateEndDate($(this).datepicker("getDate"));
+    });
+  
+    // Function to update the start date
+    function updateStartDate(date) {
+      // Convert the date to a desired format
+      var formattedDate = date.toLocaleDateString("en-US");
+  
+      // Update the span element with the new date
+      $(".startDate").text(formattedDate);
+    }
+  
+    // Function to update the end date
+    function updateEndDate(date) {
+      // Convert the date to a desired format
+      var formattedDate = date.toLocaleDateString("en-US");
+  
+      // Update the span element with the new date
+      $(".endDate").text(formattedDate);
+    }
+  });
+  
   
